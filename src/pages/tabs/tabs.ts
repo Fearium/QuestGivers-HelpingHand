d71d13b98b9c192b3ddebe05f038289aabfc90f6
Jesse Baril <jessebaril.programmer@gmail.com>
2017-04-05 16:35:55 -0400

    fixed homescreen

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -14,6 +14,7 @@ import { RollPage } from '../roll/roll';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
+  tab1Root: any = HomePage;
   tab2Root: any = JournalPage;
   tab3Root: any = RollPage;
   mySelectedIndex: number;
