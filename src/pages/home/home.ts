590665f741e5e4e4df573cb8a37159e779be4111
Jesse Baril <jessebaril.programmer@gmail.com>
2017-04-05 23:42:07 -0400

    Added Full creation functionality to Character Creation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
 import { SelectPage } from '../select/select';
 import { CreatePage } from '../create/create';
+import { RollPage } from '../roll/roll';
+import { JournalPage } from '../journal/journal';
 
 @Component({
   selector: 'page-home',
@@ -17,4 +19,14 @@ this.navCtrl.push(SelectPage);
 navToCreate(){
 this.navCtrl.push(CreatePage/*, {database: this.characters}*/);
 }
+// Actually needs to navigate to SelectPage while passing variables
+//through and then passing variables through again to edit the journal
+//of the selected character.
+navToJournalSelect(){
+this.navCtrl.push(JournalPage);
+}
+navToRoll(){
+this.navCtrl.push(RollPage);
+}
+
 }
