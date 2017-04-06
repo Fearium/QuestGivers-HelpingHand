8038522cffafbc977d73559c25af8ca83a23eefe
Jesse Baril <jessebaril.programmer@gmail.com>
2017-04-05 20:46:49 -0400

    cleaned up home.ts so that unused declarations are removed.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
-import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import { NavController} from 'ionic-angular';
 import { SelectPage } from '../select/select';
 import { CreatePage } from '../create/create';
 
@@ -10,46 +9,12 @@ import { CreatePage } from '../create/create';
 })
 export class HomePage {
 
-  characters: FirebaseListObservable<any>;
-
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, 
-  af: AngularFire, public actionSheetCtrl: ActionSheetController) {
-  this.characters = af.database.list('/characters');
+  constructor(public navCtrl: NavController) {
 }
 navToSelect(){
 this.navCtrl.push(SelectPage);
 }
 navToCreate(){
-this.navCtrl.push(CreatePage, {database: this.characters});
+this.navCtrl.push(CreatePage/*, {database: this.characters}*/);
 }
-
-createCharacter(){
-  let prompt = this.alertCtrl.create({
-    title: 'Character Name',
-    message: "Enter the information for your character",
-    inputs: [
-      {
-        name: 'name',
-        placeholder: 'Name'
-      }
-    ],
-    buttons: [
-      {
-        text: 'Cancel',
-        handler: data => {
-          console.log('Cancel clicked');
-        }
-      },
-      {
-        text: 'Save',
-        handler: data => {
-          this.characters.push({
-            name: data.name
-          });
-        }
-      }
-    ]
-  });
-  prompt.present();
-  }
 }
