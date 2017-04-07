efb86ff9389005ea9b2f8e083e94f5d67eec9d0e
patr9240 <patr9240@gmail.com>
2017-04-07 02:32:38 -0400

    Added validation to the create form, changed longer inputs to text areas.

--- src/pages/create/create.ts
+++ src/pages/create/create.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController, ActionSheetController, NavParams } from 'ionic-angular';
+
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 import { HomePage } from '../home/home';
 import { SelectPage } from '../select/select';
+
+//validator imports
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { baseStatsValidator } from '../../validators/baseStats';
+import { abilityScoreValidator } from '../../validators/abilityScore';
+import { skillsValidator } from '../../validators/skills';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'create.html'
 })
 export class CreatePage {
+
+  sectionOne: FormGroup;
+  sectionTwo: FormGroup;
+  sectionThree: FormGroup;
+  sectionFour: FormGroup;
+  sectionFive: FormGroup;
+  sectionSix: FormGroup;
 
   //NEEDS VALIDATION
 
@@ -66,15 +80,78 @@ export class CreatePage {
   characters: FirebaseListObservable<any>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-  af: AngularFire, public actionSheetCtrl: ActionSheetController) {
+  af: AngularFire, public actionSheetCtrl: ActionSheetController, public formBuilder: FormBuilder) {
+
+    this.sectionOne = formBuilder.group({
+            name: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+            class: ['', Validators.required],
+            race: ['', Validators.required],
+            alignment: ['', Validators.required],
+        });
+    
+        this.sectionTwo = formBuilder.group({
+            hp: ['', Validators.compose([Validators.required, baseStatsValidator.isValid])],
+            ac: ['', Validators.compose([Validators.required, baseStatsValidator.isValid])],
+            speed: ['', Validators.compose([Validators.required, baseStatsValidator.isValid])],
+        });
+
+        this.sectionThree = formBuilder.group({
+            str: ['', Validators.compose([Validators.required, abilityScoreValidator.isValid])],
+            strmod: ['', Validators.compose([Validators.maxLength(3), Validators.pattern('[\-\+0][0-9]*'), Validators.required])],
+            dex: ['', Validators.compose([Validators.required, abilityScoreValidator.isValid])],
+            dexmod: ['', Validators.compose([Validators.maxLength(3), Validators.pattern('[\-\+0][0-9]*'), Validators.required])],
+            con: ['', Validators.compose([Validators.required, abilityScoreValidator.isValid])],
+            conmod: ['', Validators.compose([Validators.maxLength(3), Validators.pattern('[\-\+0][0-9]*'), Validators.required])],
+            int: ['', Validators.compose([Validators.required, abilityScoreValidator.isValid])],
+            intmod: ['', Validators.compose([Validators.maxLength(3), Validators.pattern('[\-\+0][0-9]*'), Validators.required])],
+            wis: ['', Validators.compose([Validators.required, abilityScoreValidator.isValid])],
+            wismod: ['', Validators.compose([Validators.maxLength(3), Validators.pattern('[\-\+0][0-9]*'), Validators.required])],
+            cha: ['', Validators.compose([Validators.required, abilityScoreValidator.isValid])],
+            chamod: ['', Validators.compose([Validators.maxLength(3), Validators.pattern('[\-\+0][0-9]*'), Validators.required])],
+        });
+
+        this.sectionFour = formBuilder.group({
+            athletics: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            acrobatics: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            sleightofhand: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            stealth: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            arcana: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            history: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            investigation: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            nature: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            religion: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            animalhandling: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            insight: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            medicine: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            perception: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            survival: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            deception: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            intimidation: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            performance: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+            persuasion: ['', Validators.compose([Validators.required, skillsValidator.isValid])],
+        });
+
+        this.sectionFive = formBuilder.group({
+          senses: ['', Validators.required],
+          languages: ['', Validators.required],
+          passives: ['', Validators.required],
+        });
+
+        this.sectionSix = formBuilder.group({
+          weapons: ['', Validators.required],
+          equipment: ['', Validators.required],
+        });
+
   this.characters = af.database.list('/characters');
 }
 cancelCreation(){
   this.navCtrl.push(HomePage);
 }
 nextToAbilityScoresAndModifiersButton(){
-  this.showAbilityScoresAndModifiers = true;
-  this.showBasicInformation = false;
+  if(this.sectionOne.valid && this.sectionTwo.valid){ 
+      this.showAbilityScoresAndModifiers = true;
+      this.showBasicInformation = false;
+    }
 }
 previousToShowBasicInformationButton(){
   this.showAbilityScoresAndModifiers = false;
@@ -82,8 +159,10 @@ previousToShowBasicInformationButton(){
 }
 
 nextToSkillsButton(){
-  this.showSkills = true;
-  this.showAbilityScoresAndModifiers = false;
+  if(this.sectionThree.valid){ 
+    this.showSkills = true;
+    this.showAbilityScoresAndModifiers = false;
+  }
 }
 
 previousToShowAbilityScoresAndModifiersButton(){
@@ -91,68 +170,74 @@ previousToShowAbilityScoresAndModifiersButton(){
   this.showAbilityScoresAndModifiers = true;
 }
 nextToShowAbilitiesButton(){
-  this.showAbilities = true;
-  this.showSkills = false;
+  if(this.sectionFour.valid){ 
+    this.showAbilities = true;
+    this.showSkills = false;
+  }
 }
 previousToShowSkillsButton(){
   this.showAbilities = false;
   this.showSkills = true;
 }
 nextToShowGearButton(){
-  this.showGear = true;
-  this.showAbilities = false;
+  if(this.sectionFive.valid){ 
+    this.showGear = true;
+    this.showAbilities = false;
+  }
 }
 previousToShowAbilitiesButton(){
   this.showGear = false;
   this.showAbilities = true;
 }
 addCharacter(){
-  this.characters.push({
-    name: this.name,
-    hitPoints: this.hitPoints,
-    race: this.race,
-    class: this.class,
-    alignment: this.alignment,
-    armourClass: this.armourClass,
-    speed: this.speed,
-    STR: this.STR,
-    STRMod: this.STRMod,
-    DEX: this.DEX,
-    DEXMod: this.DEXMod,
-    CON: this.CON,
-    CONMod: this.CONMod,
-    INT: this.INT,
-    INTMod: this.INTMod,
-    WIS: this.WIS,
-    WISMod: this.WISMod,
-    CHA: this.CHA,
-    CHAMod: this.CHAMod,
-    athletics: this.athletics,
-    acrobatics: this.acrobatics,
-    sleightOfHand: this.sleightOfHand,
-    stealth: this.stealth,
-    arcana: this.arcana,
-    history: this.history,
-    investigation: this.investigation,
-    nature: this.nature,
-    religion: this.religion,
-    animalHandling: this.animalHandling,
-    insight: this.insight,
-    medicine: this.medicine,
-    survival: this.survival,
-    deception: this.deception,
-    intimidation: this.intimidation,
-    performance: this.performance,
-    persuasion: this.persuasion,
-    senses: this.senses,
-    languages: this.languages,
-    weapons: this.weapons,
-    equipment: this.equipment,
-    passives: this.passives,
-    journal: this.journal
-  })
-  // Will eventually navigate to Stat Block view instead
-  this.navCtrl.push(SelectPage/*, {database: this.characters}*/);
+  if(this.sectionOne.valid && this.sectionTwo.valid && this.sectionThree.valid && this.sectionFour.valid && this.sectionFive.valid && this.sectionSix.valid){ 
+    this.characters.push({
+      name: this.name,
+      hitPoints: this.hitPoints,
+      race: this.race,
+      class: this.class,
+      alignment: this.alignment,
+      armourClass: this.armourClass,
+      speed: this.speed,
+      STR: this.STR,
+      STRMod: this.STRMod,
+      DEX: this.DEX,
+      DEXMod: this.DEXMod,
+      CON: this.CON,
+      CONMod: this.CONMod,
+      INT: this.INT,
+      INTMod: this.INTMod,
+      WIS: this.WIS,
+      WISMod: this.WISMod,
+      CHA: this.CHA,
+      CHAMod: this.CHAMod,
+      athletics: this.athletics,
+      acrobatics: this.acrobatics,
+      sleightOfHand: this.sleightOfHand,
+      stealth: this.stealth,
+      arcana: this.arcana,
+      history: this.history,
+      investigation: this.investigation,
+      nature: this.nature,
+      religion: this.religion,
+      animalHandling: this.animalHandling,
+      insight: this.insight,
+      medicine: this.medicine,
+      survival: this.survival,
+      deception: this.deception,
+      intimidation: this.intimidation,
+      performance: this.performance,
+      persuasion: this.persuasion,
+      senses: this.senses,
+      languages: this.languages,
+      weapons: this.weapons,
+      equipment: this.equipment,
+      passives: this.passives,
+      journal: this.journal
+    })
+    // Will eventually navigate to Stat Block view instead
+    this.navCtrl.push(SelectPage/*, {database: this.characters}*/);
+  }
 }
 
 }
---
+++ src/validators/baseStats.ts
@@ -0,0 +1,34 @@
+import { FormControl } from '@angular/forms';
+ 
+export class baseStatsValidator {
+ 
+    static isValid(control: FormControl): any {
+ 
+        if(isNaN(control.value)){
+            return {
+                "Not a number.": true
+            };
+        }
+ 
+        if(control.value % 1 !== 0){
+            return {
+                "Not a whole number.": true
+            };
+        }
+ 
+        if(control.value < 0){
+            return {
+                "Not a positive number.": true
+            };
+        }
+ 
+        if (control.value > 99){
+            return {
+                "Too large.": true
+            };
+        }
+ 
+        return null;
+    }
+ 
+}
