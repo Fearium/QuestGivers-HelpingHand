3954b39ed668ff2d1ecfcd81486dcae640b650d2
Jesse Baril <jessebaril.programmer@gmail.com>
2017-04-11 23:23:43 -0400

    Added toggles to view page

--- src/pages/viewStats/viewStats.ts
+++ src/pages/viewStats/viewStats.ts
@@ -13,6 +13,16 @@ export class ViewStatsPage {
 
   characters: FirebaseListObservable<any>;
 
+  showSkillsToggle: boolean = true;
+  hideSkillsToggle: boolean = false;
+  skillsBlock: boolean = false;
+  showAbilitiesToggle: boolean = true;
+  hideAbilitiesToggle: boolean = false;
+  abilitiesBlock: boolean = false;
+  showGearToggle: boolean = true;
+  hideGearToggle: boolean = false;
+  gearBlock: boolean = false;
+
   constructor(public navCtrl: NavController, public params: NavParams, public alertCtrl: AlertController, 
   af: AngularFire, public actionSheetCtrl: ActionSheetController) {
       this.selectedCharacter = params.get("selectedCharacter");
@@ -20,6 +30,36 @@ export class ViewStatsPage {
       this.pluralName = this.plural();
   }
 
+  showSkillsBlock(){
+    this.showSkillsToggle = false;
+    this.hideSkillsToggle = true;
+    this.skillsBlock = true;
+  }
+  hideSkillsBlock(){
+    this.showSkillsToggle = true;
+    this.hideSkillsToggle = false;
+    this.skillsBlock = false;
+  }
+    showAbilitiesBlock(){
+    this.showAbilitiesToggle = false;
+    this.hideAbilitiesToggle = true;
+    this.abilitiesBlock = true;
+  }
+  hideAbilitiesBlock(){
+    this.showAbilitiesToggle = true;
+    this.hideAbilitiesToggle = false;
+    this.abilitiesBlock = false;
+  }
+    showGearBlock(){
+    this.showGearToggle = false;
+    this.hideGearToggle = true;
+    this.gearBlock = true;
+  }
+  hideGearBlock(){
+    this.showGearToggle = true;
+    this.hideGearToggle = false;
+    this.gearBlock = false;
+  }
   public plural(){
     var full = this.selectedCharacter.name;
     var last = (full[full.length -1]);
